444b0e6437ecfbc447b76e97535318182747e1fd
Sufyan <muhammadsufyan11011@gmail.com>
2024-11-07 21:56:39 +0500

    this is the class inheeritence

---
+++ class.py
@@ -0,0 +1,16 @@
+# class Student:
+# 	def __init__(slef,name):
+# 		slef.name=name
+# s1=Student("allia")
+# del s1.name
+# print(s1.name)
+# 
+
+
+class Account:
+	def __init__(self,acc_no,acc_pasword):
+		self.acc_no=acc_no
+		self.acc_pasword=acc_pasword
+acc1=Account("343","abcd")
+print(acc1.acc_no)
+print(acc1.acc_pasword)
